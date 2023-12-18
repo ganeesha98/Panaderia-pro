979484f806e0ac2192a858a6fdd5022525755538
Ganeesha Gunathilaka <64259697+ganeesha98@users.noreply.github.com>
2023-12-18 16:00:54 +0530

    update

--- Form/Inventory/amount.aspx.cs
+++ Form/Inventory/amount.aspx.cs
@@ -124,40 +124,45 @@ namespace Panaderia.Form.Inventory
         protected void Button2_Click(object sender, EventArgs e)
         {
             dt = (DataTable)Session["data"];
-            DataRow dr;
-            dr = dt.NewRow();
-            if (dt.Rows.Count > 0)
-            {
-                dr["Line"] = TextBox1.Text;
-                dr["item_code"] = TextBox2.Text;
-                dr["Description"] = TextBox4.Text;
-                dr["price"] = TextBox5.Text;
-                dr["psize"] = TextBox6.Text;
-                dr["packs"] = TextBox7.Text;
-                dr["nos"] = TextBox8.Text;
-                dr["discount"] = TextBox9.Text;
-                totalprice = Convert.ToInt64(TextBox8.Text) * Convert.ToInt64(TextBox5.Text);
-                dr["Amount"] = totalprice.ToString();
-            }
-            else
-            {
-                dr["Line"] = TextBox1.Text;
-                dr["item_code"] = TextBox2.Text;
-                dr["Description"] = TextBox4.Text;
-                dr["price"] = TextBox5.Text;
-                dr["psize"] = TextBox6.Text;
-                dr["packs"] = TextBox7.Text;
-                dr["nos"] = TextBox8.Text;
-                dr["discount"] = TextBox9.Text;
-                totalprice = Convert.ToInt64(TextBox8.Text) * Convert.ToInt64(TextBox5.Text);
-                dr["Amount"] = totalprice.ToString();
+            DataRow dr = dt.NewRow();
 
+            // Set values for the new row
+            dr["Line"] = TextBox1.Text;
+            dr["item_code"] = TextBox2.Text;
+            dr["Description"] = TextBox4.Text;
+            dr["price"] = TextBox5.Text;
+            dr["psize"] = TextBox6.Text;
+            dr["packs"] = TextBox7.Text;
+            dr["nos"] = TextBox8.Text;
+
+            // Check if a discount is entered
+            if (!string.IsNullOrEmpty(TextBox9.Text))
+            {
+                int discount = Convert.ToInt32(TextBox9.Text);
+                dr["discount"] = discount.ToString(); // Set the discount value in the "discount" column
             }
 
+            // Calculate total price
+            totalprice = Convert.ToInt64(TextBox8.Text) * Convert.ToInt64(TextBox5.Text);
+
+            // Apply the discount to totalprice if applicable
+            if (!string.IsNullOrEmpty(TextBox9.Text))
+            {
+                int discount = Convert.ToInt32(TextBox9.Text);
+                totalprice -= (totalprice * discount) / 100;
+            }
+
+            dr["Amount"] = totalprice.ToString();
+
+            // Add the row to the DataTable
             dt.Rows.Add(dr);
+
+            // Update GridView and session
             GridView1.DataSource = dt;
             GridView1.DataBind();
             Session["buyitems"] = dt;
+
+            // Reset input fields and recalculate sum
             TextBox1.Text = (dt.Rows.Count + 1).ToString();
             TextBox3.Text = "";
             TextBox2.Text = "";
@@ -166,8 +171,10 @@ namespace Panaderia.Form.Inventory
             TextBox6.Text = "";
             TextBox7.Text = "";
             TextBox8.Text = "";
-            
+            TextBox9.Text = ""; // Clear discount TextBox
+            calculateSum();
         }
+
         private void calculateSum()
         {
             Int32 grandtotal = 0;
@@ -184,47 +191,50 @@ namespace Panaderia.Form.Inventory
             GridView1.FooterRow.Cells[9].Font.Bold = true;
             // Label1.Text = "Price(in Words) " + ConvertNumbertoWords(grandtotal);
 
-        }
-      /*  public static string ConvertNumbertoWords(int number)
-        {
-            if (number == 0)
-                return "ZERO";
-            if (number < 0)
-                return "minus " + ConvertNumbertoWords(Math.Abs(number));
-            string words = "";
-            if ((number / 100000) > 0)
-            {
-                words += ConvertNumbertoWords(number / 100000) + " Lacs ";
-                number %= 100000;
-            }
-            if ((number / 1000) > 0)
-            {
-                words += ConvertNumbertoWords(number / 1000) + " Thousand ";
-                number %= 1000;
-            }
-            if ((number / 100) > 0)
-            {
-                words += ConvertNumbertoWords(number / 100) + " Hundred ";
-                number %= 100;
-            }
-            if (number > 0)
-            {
-                if (words != "")
-                    words += "AND ";
-                var unitsMap = new[] { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
-                var tensMap = new[] { "Zero", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
+            // Set the total amount in the txtamount TextBox
+            txtamount.Text = grandtotal.ToString();
 
-                if (number < 20)
-                    words += unitsMap[number];
-                else
-                {
-                    words += tensMap[number / 10];
-                    if ((number % 10) > 0)
-                        words += " " + unitsMap[number % 10];
-                }
-            }
-            return words;
-        }*/
+        }
+        /*public static string ConvertNumbertoWords(int number)
+          {
+              if (number == 0)
+                  return "ZERO";
+              if (number < 0)
+                  return "minus " + ConvertNumbertoWords(Math.Abs(number));
+              string words = "";
+              if ((number / 100000) > 0)
+              {
+                  words += ConvertNumbertoWords(number / 100000) + " Lacs ";
+                  number %= 100000;
+              }
+              if ((number / 1000) > 0)
+              {
+                  words += ConvertNumbertoWords(number / 1000) + " Thousand ";
+                  number %= 1000;
+              }
+              if ((number / 100) > 0)
+              {
+                  words += ConvertNumbertoWords(number / 100) + " Hundred ";
+                  number %= 100;
+              }
+              if (number > 0)
+              {
+                  if (words != "")
+                      words += "AND ";
+                  var unitsMap = new[] { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
+                  var tensMap = new[] { "Zero", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
+
+                  if (number < 20)
+                      words += unitsMap[number];
+                  else
+                  {
+                      words += tensMap[number / 10];
+                      if ((number % 10) > 0)
+                          words += " " + unitsMap[number % 10];
+                  }
+              }
+              return words;
+          }*/
 
         protected void btnSave_Click(object sender, EventArgs e)
         {
@@ -245,5 +255,29 @@ namespace Panaderia.Form.Inventory
         {
 
         }
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            // Get the index of the row being deleted
+            int rowIndex = e.RowIndex;
+
+            // Retrieve the DataTable from the session
+            DataTable dt = (DataTable)Session["data"];
+
+            // Remove the row from the DataTable
+            dt.Rows.RemoveAt(rowIndex);
+
+            // Update the session variable
+            Session["buyitems"] = dt;
+
+            // Bind the DataTable to the GridView
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+
+            // Recalculate the total sum
+            calculateSum();
+        }
+
+
+
     }
-    }
+}
